ae8323e0c3abaa645dad23344c14f2ff344635b4
miguelphb15 <miguelestudophb@gmail.com>
2023-11-23 20:22:58 -0300

    meu arquivo programado e enviado

---
+++ python.py
@@ -0,0 +1,6 @@
+print('olá amigo')
+n = int(input('Você tem quantos anos?\n'))
+if n >= 18 :
+    print('já que você tem {} então você pode servir'.format(n))
+else:
+    print('não está apto ainda.')
